0465def6a15e3f546950e06725608bcfa62de222
Hanchen Li <li1191@purdue.edu>
2014-07-22 21:51:54 +0800

    algorithm, dialog

--- src/com/lhc/lhc2048/GameSystem.java
+++ src/com/lhc/lhc2048/GameSystem.java
@@ -1,56 +1,261 @@
 package com.lhc.lhc2048;
 
+import java.util.Random;
+
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.backup.RestoreObserver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.view.View;
 
 public class GameSystem {
 	private int data[][];
-	boolean lose;
-	int score,biggest;
+	private TextView blocks[][];
+	private TextView labelScore;
+	private boolean lose;
+	private int score,max,nums;
+	private String strScore;
+	private Random rand;
+	private Context context;
 	
 	public boolean isLose() {
 		return lose;
 	}
 
-	public GameSystem(){
-		
+	public GameSystem(Activity main){
+		rand=new Random();
+		context=main;
+		blocks=new TextView[4][4];
+		blocks[0][0]=(TextView)main.findViewById(R.id.num11);
+		blocks[0][1]=(TextView)main.findViewById(R.id.num12);
+		blocks[0][2]=(TextView)main.findViewById(R.id.num13);
+		blocks[0][3]=(TextView)main.findViewById(R.id.num14);
+		blocks[1][0]=(TextView)main.findViewById(R.id.num21);
+		blocks[1][1]=(TextView)main.findViewById(R.id.num22);
+		blocks[1][2]=(TextView)main.findViewById(R.id.num23);
+		blocks[1][3]=(TextView)main.findViewById(R.id.num24);
+		blocks[2][0]=(TextView)main.findViewById(R.id.num31);
+		blocks[2][1]=(TextView)main.findViewById(R.id.num32);
+		blocks[2][2]=(TextView)main.findViewById(R.id.num33);
+		blocks[2][3]=(TextView)main.findViewById(R.id.num34);
+		blocks[3][0]=(TextView)main.findViewById(R.id.num41);
+		blocks[3][1]=(TextView)main.findViewById(R.id.num42);
+		blocks[3][2]=(TextView)main.findViewById(R.id.num43);
+		blocks[3][3]=(TextView)main.findViewById(R.id.num44);
+		labelScore=(TextView)main.findViewById(R.id.labelScore);
+		strScore=main.getString(R.string.score);
 		restart();
 	}
 	
 	public void restart(){
 		lose=false;
-		score=4;
-		biggest=2;
+		score=0;
+		max=0;
+		nums=0;
+		rand=new Random();
 		data=new int[4][4];
+		createNewBlock(true);
+		createNewBlock(true);
+		display();
 	}
 	
 	public void display(){
-		
+		for(int i=0;i<4;i++)
+			for(int j=0;j<4;j++)
+				if (data[i][j]==0) 
+					blocks[i][j].setText("");
+				else
+					blocks[i][j].setText(Integer.toString(data[i][j]));
+		labelScore.setText(strScore+'\n'+score);
 	}
 
 	public boolean up(){
-		Log.e("move", "u");
-		return true;
+		if(lose) return false;
+		boolean changed=false;
+		int current,j;
+		for(int i=0;i<4;i++){
+			//数字上提
+			for(current=0;current<3;current++){
+				for(j=current;j<4;j++) if(data[j][i]!=0) break;
+				if((j!=current)&&(j!=4)){
+					changed=true;
+					data[current][i]=data[j][i];
+					data[j][i]=0;
+				}
+			}
+			//数字合并
+			for(current=0;current<3;current++){
+				if(data[current][i]==data[current+1][i]){
+					changed=true;
+					nums--;
+					data[current][i]*=2;
+					if (data[current][i]>max) max=data[current][i];
+					for(j=current+1;j<3;j++) data[j][i]=data[j+1][i];
+					data[3][i]=0;
+				}
+			}
+		}
+		if ((!changed)&&(nums==16)) {
+			lose();
+			return false;
+		}
+		if (changed) {
+			createNewBlock(false);
+			display();
+		}
+		return changed;
 	}
 
 	public boolean down(){
-		Log.e("move", "d");
-		return true;
+		if(lose) return false;
+		boolean changed=false;
+		int current,j;
+		for(int i=3;i>=0;i--){
+			//数字下提
+			for(current=3;current>0;current--){
+				for(j=current;j>=0;j--) if(data[j][i]!=0) break;
+				if((j!=current)&&(j!=-1)){
+					changed=true;
+					data[current][i]=data[j][i];
+					data[j][i]=0;
+				}
+			}
+			//数字合并
+			for(current=3;current>0;current--){
+				if(data[current][i]==data[current-1][i]){
+					changed=true;
+					nums--;
+					data[current][i]*=2;
+					if (data[current][i]>max) max=data[current][i];
+					for(j=current-1;j>0;j--) data[j][i]=data[j-1][i];
+					data[0][i]=0;
+				}
+			}
+		}
+		if ((!changed)&&(nums==16)) {
+			lose();
+			return false;
+		}
+		if (changed) {
+			createNewBlock(false);
+			display();
+		}
+		return changed;
 	}
 
 	public boolean left(){
-		Log.e("move", "l");
-		return true;
+		if(lose) return false;
+		boolean changed=false;
+		int current,j;
+		for(int i=0;i<4;i++){
+			//数字左提
+			for(current=0;current<3;current++){
+				for(j=current;j<4;j++) if(data[i][j]!=0) break;
+				if((j!=current)&&(j!=4)){
+					changed=true;
+					data[i][current]=data[i][j];
+					data[i][j]=0;
+				}
+			}
+			//数字合并
+			for(current=0;current<3;current++){
+				if(data[i][current]==data[i][current+1]){
+					changed=true;
+					nums--;
+					data[i][current]*=2;
+					if (data[i][current]>max) max=data[i][current];
+					for(j=current+1;j<3;j++) data[i][j]=data[i][j+1];
+					data[i][3]=0;
+				}
+			}
+		}
+		if ((!changed)&&(nums==16)) {
+			lose();
+			return false;
+		}
+		if (changed) {
+			createNewBlock(false);
+			display();
+		}
+		return changed;
 	}
 
 	public boolean right(){
-		Log.e("move", "r");
-		return true;
+		if(lose) return false;
+		boolean changed=false;
+		int current,j;
+		for(int i=3;i>=0;i--){
+			//数字右提
+			for(current=3;current>0;current--){
+				for(j=current;j>=0;j--) if(data[i][j]!=0) break;
+				if((j!=current)&&(j!=-1)){
+					changed=true;
+					nums--;
+					data[i][current]=data[i][j];
+					data[i][j]=0;
+				}
+			}
+			//数字合并
+			for(current=3;current>0;current--){
+				if(data[i][current]==data[i][current-1]){
+					changed=true;
+					data[i][current]*=2;
+					if (data[i][current]>max) max=data[i][current];
+					for(j=current-1;j>0;j--) data[i][j]=data[i][j-1];
+					data[i][0]=0;
+				}
+			}
+		}
+		if ((!changed)&&(nums==16)) {
+			lose();
+			return false;
+		}
+		if (changed) {
+			createNewBlock(false);
+			display();
+		}
+		return changed;
 	}
 	
-	private void createNewBlock(){
-		
+	private void createNewBlock(boolean always2){
+		nums++;
+		int x,y;
+		do{
+			x=rand.nextInt(4);
+			y=rand.nextInt(4);
+			Log.i("rand",""+x+','+y);
+		}while(data[x][y]!=0);
+		Log.i("final",""+x+','+y);
+		if ((rand.nextInt(10)!=0)||always2){
+			data[x][y]=2;
+			score+=2;
+			if (max<2) max=2;
+		}else{
+			data[x][y]=4;
+			score+=4;
+			if (max<4) max=4;
+		}
 	}
 	
+	private void lose(){
+		lose=true;
+		AlertDialog alert=new AlertDialog.Builder(context).create();
+		alert.setIcon(R.drawable.info);
+		alert.setTitle(context.getString(R.string.gameOver));
+		alert.setMessage(context.getString(R.string.yourScore)+score+','+context.getString(R.string.maxNum)+max);
+		alert.setButton(DialogInterface.BUTTON_NEGATIVE,context.getString(R.string.restart),new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				restart();
+			}
+		});
+		alert.setButton(DialogInterface.BUTTON_NEUTRAL,context.getString(R.string.ok),new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+		alert.show();
+	}
 }
--- src/com/lhc/lhc2048/MainActivity.java
+++ src/com/lhc/lhc2048/MainActivity.java
@@ -2,6 +2,8 @@ package com.lhc.lhc2048;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.gesture.GestureOverlayView;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +12,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	GameSystem gs;
@@ -24,7 +29,28 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		gs=new GameSystem();
+		gs=new GameSystem(this);
+		Button butReset=(Button)findViewById(R.id.butReset);
+		butReset.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog alert=new AlertDialog.Builder(MainActivity.this).create();
+				alert.setIcon(R.drawable.info);
+				alert.setTitle(MainActivity.this.getString(R.string.restart));
+				alert.setMessage(MainActivity.this.getString(R.string.sureRestart));
+				alert.setButton(DialogInterface.BUTTON_NEGATIVE,MainActivity.this.getString(R.string.cancel),new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						
+					}
+				});
+				alert.setButton(DialogInterface.BUTTON_NEUTRAL,MainActivity.this.getString(R.string.ok),new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						MainActivity.this.gs.restart();
+					}
+				});
+				alert.show();
+			}
+		});
+		butReset.setClickable(true);
 		gd=new GestureDetector(MainActivity.this,new GestureDetector.SimpleOnGestureListener(){
 			public boolean onFling(MotionEvent e1, MotionEvent e2,float velocityX, float velocityY) {
 				float x1=e1.getX();
@@ -32,9 +58,10 @@ public class MainActivity extends Activity {
 				float y1=e1.getY();
 				float y2=e2.getY();
 				if ((x2-x1>75)&&(Math.abs(y1-y2)<(x2-x1)/2.5)) gs.right();
-				if ((x1-x2>75)&&(Math.abs(y1-y2)<(x1-x2)/2.5)) gs.left();
-				if ((y2-y1>75)&&(Math.abs(x1-x2)<(y2-y1)/2.5)) gs.down();
-				if ((y1-y2>75)&&(Math.abs(x1-x2)<(y1-y2)/2.5)) gs.up();
+				else if ((x1-x2>75)&&(Math.abs(y1-y2)<(x1-x2)/2.5)) gs.left();
+				else if ((y2-y1>75)&&(Math.abs(x1-x2)<(y2-y1)/2.5)) gs.down();
+				else if ((y1-y2>75)&&(Math.abs(x1-x2)<(y1-y2)/2.5)) gs.up();
+				else Toast.makeText(MainActivity.this, getString(R.string.unrecognizableGesture), Toast.LENGTH_SHORT).show();
 				return super.onFling(e1, e2, velocityX, velocityY);
 			}
 		});
@@ -48,6 +75,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -58,8 +86,22 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		if (id == R.id.action_restart) {
+			AlertDialog alert=new AlertDialog.Builder(this).create();
+			alert.setIcon(R.drawable.info);
+			alert.setTitle(getString(R.string.restart));
+			alert.setMessage(this.getString(R.string.sureRestart));
+			alert.setButton(DialogInterface.BUTTON_NEGATIVE,getString(R.string.cancel),new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					
+				}
+			});
+			alert.setButton(DialogInterface.BUTTON_NEUTRAL,getString(R.string.ok),new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					gs.restart();
+				}
+			});
+			alert.show();
 		}
 		return super.onOptionsItemSelected(item);
 	}
